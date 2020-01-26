6c169ff78c5f98b6b722e76798f48165b52b3182
AdiLew <adilu.lev@gmail.com>
2020-01-26 16:17:34 +0200

    actions moved under middlewear, setFilters works!

--- src/utils/telegram/scenes/getUrl-scene.js
+++ src/utils/telegram/scenes/getUrl-scene.js
@@ -2,9 +2,18 @@ const Scene = require('telegraf/scenes/base'),
     Stage = require('telegraf/stage')
 
 const getUrl = new Scene('getUrl')
-getUrl.enter(ctx => ctx.reply('Send me your search URL please'));
-getUrl.leave(ctx => ctx.reply('Alright!'))
-getUrl.hears(/cancel/gi, Stage.leave())
+getUrl.enter(ctx =>
+    ctx.reply('Send me your search URL please')
+);
+
+getUrl.leave(ctx =>
+    ctx.reply('Alright!')
+)
+
+getUrl.hears(/cancel/gi,
+    Stage.leave()
+)
+
 getUrl.on('message', ctx => {
     return console.log(ctx.message)
 })
--- src/utils/telegram/telegram.js
+++ src/utils/telegram/telegram.js
@@ -1,38 +1,37 @@
 const Telegraf = require('telegraf'),
-  session = require('telegraf/session')
-
+  session = require('telegraf/session'),
+  stage = require('./stage/stage'),
+  startCommand = require('./commands/start-command')
 
 const token = process.env.BOT_TOKEN;
 const bot = new Telegraf(token);
-const stage = require('./stage/stage')
-
-const startCommand = require('./commands/start-command')
-
-
 
 
 // Event Handlers
 bot.start(startCommand)
 
+// bot.action('setFilters', async (ctx) =>
+//   ctx.scene.enter('getUrl')
+// )
 
-
-bot.action('setFilters', async (ctx) => ctx.scene.enter('getUrl')
-)
-
-
+/*
 bot.action('urlFilters', async (ctx) => {
   ctx.reply('Alright! Please to go ')
-})
+})*/
 
 
-bot.action(/.+/, (ctx) => {
-  console.log(ctx);
-  return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`)
-})
+
 
 bot.use(session())
 bot.use(stage.middleware())
 
+bot.action('setFilters', async (ctx) =>
+  ctx.scene.enter('getUrl')
+)
+bot.action(/.+/, (ctx) => {
+  console.log(ctx);
+  return ctx.answerCbQuery(`Oh, ${ctx.match[0]}! Great choice`)
+})
 
 
 module.exports = bot;
