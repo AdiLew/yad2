f2d1ae93a918a0b7074302b94882a28ebd92c090
adilew <adilu.lev@gmail.com>
2020-01-26 11:14:22 +0200

    :tada: first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('./db/mongoose');
+const bot = require('./utils/telegram');
+
+const app = express();
+const port = process.env.PORT || 3300;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`The server is up on port ${port}`);
+})
+
+
+//bot.launch();
+bot.startPolling()
---
+++ src/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require ('mongoose');
+
+const userSchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true,
+        unique: true
+    },
+    userName:{
+        type: String,
+        trim: true,
+        lowercase: true
+    },
+    firstName: {
+        type: String,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        trim: true
+    },
+    isBot: {
+        type: Boolean
+    },
+    lastUsed:{
+        type: Date,
+        default: new Date()
+    },
+    created: {
+        type: Date,
+        default: new Date()
+    }
+})
+
+userSchema.methods.updateLastUsed = async function(){
+    const user = this;
+    user.lastUsed = new Date();
+    user.markModified('lastUsed');
+    await user.save();
+}
+
+// userSchema.statics.findUserbyId = async (userId) => {
+//     const user = await this.findOne({userId});
+//     return user;
+// }
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
