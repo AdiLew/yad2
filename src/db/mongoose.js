d19d2981ac2b2a132e3c8d5a4265562868576ae5
AdiLew <adilu.lev@gmail.com>
2020-01-26 14:02:47 +0200

    Mongo conneciton string as env var

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017', {
+mongoose.connect(process.env.DB_CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     dbName: 'yad2-data'
-})
+}).catch(err =>
+    console.log(err)
+)
